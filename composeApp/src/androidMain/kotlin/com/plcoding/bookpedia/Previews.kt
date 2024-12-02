050272ee960780645bd1536321b4bbdfb9e52d9d
João Pedro <joaoplay16@gmail.com>
2024-12-01 23:30:41 -0300

    Create the `BookListScreenPreview`

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -1,9 +1,13 @@
 package com.plcoding.bookpedia
 
+import android.R.attr.rating
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.presentation.book_list.BookListScreen
+import com.plcoding.bookpedia.book.presentation.book_list.BookListState
 import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
 
 @Preview
@@ -18,4 +22,31 @@ fun BookSearchBarPreview() {
             )
         }
     }
+}
+
+private val books = (1..100).map {
+    Book(
+        id = it.toString(),
+        title = "Book $it",
+        authors = listOf("Otimista"),
+        imageUrl = "http://test.com/image.jpg",
+        description = "Description $it",
+        languages = emptyList(),
+        firstPublishYear = null,
+        averageRating = 4.67855,
+        ratingCount = 5,
+        numPages = 100,
+        numEditions = 3
+    )
+}
+
+@Preview
+@Composable
+fun BookListScreenPreview() {
+    BookListScreen(
+        state = BookListState(
+            searchResults = books
+        ),
+        onAction = {}
+    )
 }
