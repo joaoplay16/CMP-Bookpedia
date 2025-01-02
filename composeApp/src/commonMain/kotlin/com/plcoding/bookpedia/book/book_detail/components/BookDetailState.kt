81fcce8ede2205e0ed783da7e7af38c4cd570280
Joao <joaoplay16@gmail.com>
2025-01-01 22:38:58 -0300

    Create data class BookDetailState

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/book_detail/components/BookDetailState.kt
@@ -0,0 +1,9 @@
+package com.plcoding.bookpedia.book.book_detail.components
+
+import com.plcoding.bookpedia.book.domain.Book
+
+data class BookDetailState(
+    val isLoading: Boolean = false,
+    val isFavorite: Boolean = false,
+    val book: Book? = null,
+)
