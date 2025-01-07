a900bfc62316883b8d39bec5ee061e84dd0052be
João Pedro <joaoplay16@gmail.com>
2025-01-06 22:00:15 -0300

    Set up local database

--- composeApp/src/iosMain/kotlin/com/plcoding/bookpedia/di/Modules.ios.kt
+++ composeApp/src/iosMain/kotlin/com/plcoding/bookpedia/di/Modules.ios.kt
@@ -1,5 +1,6 @@
 package com.plcoding.bookpedia.di
 
+import com.plcoding.bookpedia.book.data.database.DatabaseFactory
 import io.ktor.client.engine.darwin.Darwin
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -7,4 +8,5 @@ import org.koin.dsl.module
 actual val platformModule: Module
     get() = module {
         single{ Darwin.create() }
+        single { DatabaseFactory() }
     }
