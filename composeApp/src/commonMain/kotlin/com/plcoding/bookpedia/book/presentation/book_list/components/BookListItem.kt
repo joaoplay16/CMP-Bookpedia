686fffd7cd94e1661a09a3031907b3a0a4a328e0
João Pedro <joaoplay16@gmail.com>
2025-01-10 23:37:59 -0300

    Create and implement PulseAnimation

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookListItem.kt
@@ -38,6 +38,7 @@ import cmp_bookpedia.composeapp.generated.resources.book_error_2
 import coil3.compose.rememberAsyncImagePainter
 import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.core.presentation.LightBlue
+import com.plcoding.bookpedia.core.presentation.PulseAnimation
 import com.plcoding.bookpedia.core.presentation.SandYellow
 import org.jetbrains.compose.resources.painterResource
 import kotlin.math.round
@@ -89,7 +90,9 @@ fun BookListItem(
                 )
 
                 when (val result = imageLoadResult) {
-                    null -> CircularProgressIndicator()
+                    null -> PulseAnimation(
+                        modifier = Modifier.size(60.dp),
+                    )
                     else -> {
                         Image(
                             painter = if (result.isSuccess) painter else {
